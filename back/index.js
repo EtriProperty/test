e7cffbace353e13810350a21452b230b6a2964dc
Opdata <pb03641@gmail.com>
2019-12-11 15:46:52 +0900

    [test] mysql connection test

--- back/index.js
+++ back/index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root1234",
+  port: 3306,
+  database: "my_db"
+});
+
+connection.connect();
+
+connection.query("SELECT * from Persons", function(err, rows, fields) {
+  if (!err) console.log("The solution is : ", rows);
+  else console.log("Error", err);
+});
+
+connection.end();
