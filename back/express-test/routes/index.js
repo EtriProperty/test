c660a7864ab278b20bf6ac67576ed15ef63ad444
Opdata <pb03641@gmail.com>
2019-12-17 10:18:23 +0900

    [test] get, post 기본 소스만 추가

--- back/express-test/routes/index.js
+++ back/express-test/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* Base get post method ...*/
+router.post("/", function(req, res, next) {
+  console.log("post 메소드의 / 주소 때만 실행");
+});
+
+router.get("/", function(req, res, next) {
+  console.log("get 메소드의 / 주소 때만 실행");
 });
 
 module.exports = router;
