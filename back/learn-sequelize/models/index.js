9f984d67d3f009e3cae2d0b37e7ee658e97188e4
Opdata <pb03641@gmail.com>
2019-12-17 22:38:46 +0900

    [test] 모델정의한거 require(모듈) 추가, 관계 정의 테스트 성공

--- back/learn-sequelize/models/index.js
+++ back/learn-sequelize/models/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "development"; // 배포시에는 confing.json에서 production으로 바꾸고 배포하면된다.
 const config = require(path.join(__dirname, "..", "config", "config.json"))[
   env
 ];
@@ -12,35 +12,44 @@ const sequelize = new Sequelize(
   config
 );
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
-
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach(file => {
-    const model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.userinfo = require("./userinfo")(sequelize, Sequelize);
+db.contentinfo = require("./contentinfo")(sequelize, Sequelize);
+db.replycontent = require("./replycontent")(sequelize, Sequelize);
+db.wishilist = require("./wishilist")(sequelize, Sequelize);
+
+//관계정의
+db.userinfo.hasMany(db.wishilist, {
+  sourceKey: "number",
+  foreignKey: "userId"
+});
+
+db.wishilist.belongsTo(db.userinfo, {
+  targetKey: "number",
+  foreignKey: "userId"
+});
+
+db.userinfo.hasMany(db.contentinfo, {
+  sourceKey: "number",
+  foreignKey: "owner_id"
+});
+
+db.contentinfo.belongsTo(db.userinfo, {
+  targetKey: "number",
+  foreignKey: "owner_id"
+});
+
+db.contentinfo.hasMany(db.wishilist, {
+  sourceKey: "number",
+  foreignKey: "homeid"
+});
+
+db.wishilist.belongsTo(db.contentinfo, {
+  targetKey: "number",
+  foreignKey: "homeid"
+});
+
+//db 객체에 userinfo라는 모델을 담았고 접근가능
 module.exports = db;
