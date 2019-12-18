9e658076b0a5e9f1535931d903e8be6674eb26bf
Opdata <pb03641@gmail.com>
2019-12-18 15:42:00 +0900

    [test] 틀에 라우터 설정 완료

--- back/learn-sequelize/app.js
+++ back/learn-sequelize/app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var loginRouter = require("./routes/login"); // 로그인 라우터 추가
+var registerRouter = require("./routes/register"); // 회원가입 라우터 추가
 var sequelize = require("./models").sequelize;
 //var sequelize = require("./models") == var sequelize = require('./models/index.js')
 
@@ -15,15 +17,18 @@ sequelize.sync(); // 서버 실행시 MySQL과 연동된다.
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.engine("html", require("pug").renderFile); // html 띄우기
 
 app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", indexRouter); //메인 api
+//app.use("/users", usersRouter); //회원가입 api로 바꿀예정
+app.use("/login", loginRouter); //로그인 api
+app.use("/register", registerRouter); //회원가입 api
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +43,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
 });
 
 module.exports = app;
