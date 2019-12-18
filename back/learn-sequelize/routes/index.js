6630a75c2350d89cfffb768a1c61286f074f8e34
Opdata <pb03641@gmail.com>
2019-12-18 15:43:21 +0900

    [feat] 페이지 로드 Get 추가 완료

--- back/learn-sequelize/routes/index.js
+++ back/learn-sequelize/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  fs.readFile("./views/homepage.html", function(err, data) {
+    // homepage.html 로드
+    if (err) {
+      console.log(err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" }); //HeadType 설정
+      res.end(data); // 로드 html response
+    }
+  });
 });
 
 module.exports = router;
