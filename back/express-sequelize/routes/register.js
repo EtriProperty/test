ac0e4699060007306697de1334787660dc3cbede
Opdata <pb03641@gmail.com>
2019-12-20 18:40:02 +0900

    [feat] DB 아이디 중복체크 API 기능 추가 완료

--- back/express-sequelize/routes/register.js
+++ back/express-sequelize/routes/register.js
@@ -22,14 +22,11 @@ router.post("/register_on", async function(req, res) {
     if (Object.keys(result).length === 0) {
       //중복 아이디가 없을떄
       let body = req.body;
-      console.log("중복 아이디없음 조건 성공");
       let salt = Math.round(new Date().valueOf() * Math.random()) + "";
       let hashPassword = crypto
         .createHash("sha512")
         .update(body.userpassword + salt)
         .digest("hex");
-
-      console.log(body);
       await user.create({
         //DB에 저장, 잘들어감
         id: body.userid,
@@ -39,28 +36,17 @@ router.post("/register_on", async function(req, res) {
         phone: body.userphonenumber,
         auth: 1
       });
-
-      /*
-      let insertinfo = async () => {
-        await user.addLabel({
-          //DB에 저장
-          //number: count,
-          id: body.userid,
-          password: hashPassword,
-          name: body.username,
-          email: body.useremail,
-          phone: body.userphonenumber,
-          auth: 1
-        });
-      };
-      */
-      // console.log("저장 소스 넘어감");
-      res.redirect("/");
+      res.redirect("/"); // 수정필요, 회원가입완료 페이지로 가야함
     } else {
-      //아이디가 이미 있을때
+      //아이디가 이미 있을때, 409 에러 던져줌
+      res.status(409).json(res.statusCode);
     }
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+});
 
-    /*
+/*
     let dbPassword = result.dataValues.password;
     let inputPassword = body.password;
     let salt = result.dataValues.salt;
@@ -81,9 +67,4 @@ router.post("/register_on", async function(req, res) {
       res.redirect("/index");
     }
     */
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
-  }
-});
-
 module.exports = router;
